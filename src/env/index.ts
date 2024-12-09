b72dd9788ad5ec863358e984ae61d5badb92f199
Gabriel <gabriel.g2d2@gmail.com>
2024-12-09 17:53:36 -0300

    feat: remove DATABASE_URL from environment schema for simplification

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,7 +9,6 @@ if (process.env.NODE_ENV === "test") {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
-  DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 });
 
