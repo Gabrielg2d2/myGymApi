9ddf293dbb11106581726e11276c42b937b2c861
Gabriel <gabriel.g2d2@gmail.com>
2024-12-13 18:17:58 -0300

    feat: add email uniqueness check and hash password in user creation

--- src/http/controllers/create-user/index.ts
+++ src/http/controllers/create-user/index.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
@@ -14,8 +15,18 @@ export async function createUserController(
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (user) {
+    return reply.status(409).send();
+  }
+
+  const password_hash = await hash(password, 6);
+
   await prisma.user.create({
-    data: { name, email, password_hash: password },
+    data: { name, email, password_hash },
   });
 
   return reply.status(201).send();
