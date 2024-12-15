48858cafcbe55b2937ed8baf6f7983034e68f056
Gabriel <gabriel.g2d2@gmail.com>
2024-12-15 09:27:16 -0300

    feat: use type alias for user creation input in UsersRepository

--- src/repositories/users-repository/index.ts
+++ src/repositories/users-repository/index.ts
@@ -1,10 +1,12 @@
 import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 
+type IDataCreate = Prisma.UserCreateInput;
+
 export class UsersRepository {
   constructor(private readonly db = prisma) {}
 
-  async create(data: Prisma.UserCreateInput) {
+  async create(data: IDataCreate) {
     const user = await this.db.user.findUnique({
       where: { email: data.email },
     });
