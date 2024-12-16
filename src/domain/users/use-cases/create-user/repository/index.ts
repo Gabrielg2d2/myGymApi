b655b3195349f4883daab952f33dccd6be727296
Gabriel <gabriel.g2d2@gmail.com>
2024-12-16 19:31:56 -0300

    feat: enhance user creation logic with error handling and standardized response structure

--- src/domain/users/use-cases/create-user/repository/index.ts
+++ src/domain/users/use-cases/create-user/repository/index.ts
@@ -5,7 +5,7 @@ import { Prisma } from "@prisma/client";
 
 type IDataCreate = Prisma.UserCreateInput;
 
-type IUser = {
+type IDataUser = {
   id: string;
   name: string;
   email: string;
@@ -16,33 +16,51 @@ type IUser = {
 export class RepositoryCreateUser {
   constructor(private readonly dbAdapter = prisma) {}
 
-  async execute(data: IDataCreate): IReturnDefaultRepository<IUser> {
-    const user = await this.dbAdapter.user.findUnique({
-      where: { email: data.email },
-    });
+  async execute(data: IDataCreate): IReturnDefaultRepository<IDataUser | null> {
+    try {
+      const user = await this.dbAdapter.user.findUnique({
+        where: { email: data.email },
+      });
 
-    if (user) {
+      if (user) {
+        throw new Error("User already exists");
+      }
+
+      const newUser = await this.dbAdapter.user.create({
+        data,
+      });
+
       return {
-        data: user,
+        data: newUser,
         message: {
-          en: "User already exists",
-          pt: "Usuário já existe",
+          en: "User created successfully",
+          pt: "Usuário criado com sucesso",
+        },
+        typeMessage: ITypeMessageGlobal.SUCCESS,
+        statusCode: 201,
+      };
+    } catch (error) {
+      if (error instanceof Error && error.message === "User already exists") {
+        return {
+          data: null,
+          message: {
+            en: error.message,
+            pt: error.message,
+          },
+          typeMessage: ITypeMessageGlobal.ERROR,
+          statusCode: 409,
+        };
+      }
+
+      return {
+        data: null,
+        message: {
+          en: "Error creating user, try again later",
+          pt: "Erro ao criar o usuário, tente novamente mais tarde",
         },
         typeMessage: ITypeMessageGlobal.ERROR,
+        statusCode: 500,
       };
     }
-
-    const newUser = await this.dbAdapter.user.create({
-      data,
-    });
-
-    return {
-      data: newUser,
-      message: {
-        en: "User created successfully",
-        pt: "Usuário criado com sucesso",
-      },
-      typeMessage: ITypeMessageGlobal.SUCCESS,
-    };
   }
 }
