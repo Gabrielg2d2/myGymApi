c62be93dc7cc990e0e2823a1dd1a2513ed6a5f18
Gabriel <gabriel.g2d2@gmail.com>
2024-12-16 20:23:29 -0300

    feat: enhance createUser use case with input validation and error handling

--- src/domain/users/use-cases/create-user/index.ts
+++ src/domain/users/use-cases/create-user/index.ts
@@ -1,18 +1,45 @@
+import { ITypeMessageGlobal } from "@/domain/global/types/type-message";
 import { hash } from "bcryptjs";
+import { z } from "zod";
 import { RepositoryCreateUser } from "./repository";
 
-export type ICreateUserUseCase = {
-  name: string;
-  email: string;
-  password: string;
-};
-
+export type ICreateUserUseCase =
+  | {
+      name: string;
+      email: string;
+      password: string;
+    }
+  | unknown;
 export class CreateUserUseCase {
   constructor(
     private readonly repositoryCreateUser = new RepositoryCreateUser()
   ) {}
 
-  async execute({ name, email, password }: ICreateUserUseCase) {
+  async execute(body: ICreateUserUseCase) {
+    const registerBodySchema = z.object({
+      name: z.string().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const isBodyValid = registerBodySchema.safeParse(body);
+
+    if (!isBodyValid.success) {
+      const dataDefault = {
+        data: null,
+        message: {
+          en: "Invalid content",
+          pt: "Conteúdo inválido",
+        },
+        typeMessage: ITypeMessageGlobal.ERROR,
+        statusCode: 400,
+      };
+
+      return dataDefault;
+    }
+
+    const { name, email, password } = isBodyValid.data;
+
     const password_hash = await hash(password, 6);
 
     return await this.repositoryCreateUser.execute({
--- src/http/controllers/create-user/index.ts
+++ src/http/controllers/create-user/index.ts
@@ -1,22 +1,13 @@
 import { UsersDomain } from "@/domain/users/main";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
 
 export async function createUserController(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const registerBodySchema = z.object({
-    name: z.string().min(3),
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
-
-  const { name, email, password } = registerBodySchema.parse(request.body);
-
   const usersDomain = new UsersDomain();
 
-  const result = await usersDomain.createUser({ name, email, password });
+  const result = await usersDomain.createUser(request.body);
 
   return reply.status(result.statusCode).send(result);
 }
