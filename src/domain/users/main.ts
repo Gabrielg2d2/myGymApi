f038425531bb8191427e9a619c18b6cca5bcc7f3
Gabriel <gabriel.g2d2@gmail.com>
2024-12-18 20:33:09 -0300

    feat: update createUser method to use Prisma.UserCreateInput for improved type safety

--- src/domain/users/main.ts
+++ src/domain/users/main.ts
@@ -1,11 +1,11 @@
+import { Prisma } from "@prisma/client";
 import { IReturnDefaultDomain } from "../global/types/return-default-domain";
 import { CreateUserUseCase, ICreateUserUseCase } from "./use-cases/create-user";
-import { IDataUser } from "./use-cases/create-user/repository";
 
 interface IUsersDomain {
   createUser(
     body: ICreateUserUseCase
-  ): Promise<IReturnDefaultDomain<IDataUser | null>>;
+  ): Promise<IReturnDefaultDomain<Prisma.UserCreateInput | null>>;
 }
 
 export class UsersDomain implements IUsersDomain {
