2e1c284a50a44b7bddabf5a518832b3716417800
Gabriel <gabriel.g2d2@gmail.com>
2024-12-19 07:51:12 -0300

    fix: update import path for createUserController to reflect new directory structure

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { createUserController } from "../controllers/create-user";
+import { createUserController } from "../controllers/users/create-user";
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post("/users", createUserController);
