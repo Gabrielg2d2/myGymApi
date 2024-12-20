f1fdfbd01dbc0859853f19e12b679cd6250305b2
Gabriel <gabriel.g2d2@gmail.com>
2024-12-20 15:40:09 -0300

    refactor: simplify CustomErrorGlobal constructor by removing redundant name property

--- src/domain/global/class-custom-error/index.ts
+++ src/domain/global/class-custom-error/index.ts
@@ -1,18 +1,15 @@
 type IPropsConstructor = {
   message: string;
   details?: unknown;
-  name?: string;
   body?: unknown;
 };
 
 export class CustomErrorGlobal extends Error {
   public details: unknown;
-  public name: string;
   public body?: unknown;
 
-  constructor({ message, details, name = "Error", body }: IPropsConstructor) {
+  constructor({ message, details, body }: IPropsConstructor) {
     super(message);
-    this.name = name;
     this.details = details;
     this.body = body;
   }
