45d32f4ccdcc8c5fd6f40e999a3f56b876fe4ff5
Gabriel <gabriel.g2d2@gmail.com>
2024-12-21 08:48:55 -0300

    test: remove obsolete password encryption test from CreateUser unit tests

--- src/domain/users/use-cases/create-user/index.unit.test.ts
+++ src/domain/users/use-cases/create-user/index.unit.test.ts
@@ -60,32 +60,6 @@ describe("Create User", () => {
         error: null,
       });
     });
-
-    test("The user's password must be encrypted", async () => {
-      const mockUserRepository = {
-        execute: vitest.fn().mockReturnValue({
-          id: "1c56ads1c65a1sc65as",
-          name: "John Doe",
-          email: "john@gmail.com",
-          password_hash: "csac1sa56c1123456",
-          created_at: "2024-12-12T21:33:33.001Z",
-        }),
-      } as unknown as RepositoryCreateUser;
-
-      const createUserUseCase = new CreateUserUseCase(mockUserRepository);
-      const newUser = await createUserUseCase.execute({
-        name: "John Doe",
-        email: "john@gmail.com",
-        password: "123456",
-      });
-
-      const passwordHash = newUser?.data?.password_hash;
-      if (!passwordHash) {
-        throw new Error("Password hash is undefined");
-      }
-
-      expect(passwordHash).not.toBe("123456");
-    });
   });
 
   describe("Error", () => {
@@ -131,16 +105,5 @@ describe("Create User", () => {
 
       expect(newUser.statusCode).toBe(409);
     });
-
-    //
-    // Não deve ser possível criar um novo usuário com um e-mail inválido
-    //   test("Should not be able to create a new user with an invalid email", async () => {});
-    //
-    // Não deve ser possível criar um novo usuário com um nome inválido
-    //   test("Should not be able to create a new user with an invalid name", async () => {});
-    //
-    // Não deve ser possível criar um novo usuário com uma senha inválida
-    //   test("Should not be able to create a new user with an invalid password", async () => {});
-    // });
   });
 });
