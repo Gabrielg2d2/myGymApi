69704a62cc970e5bf641fe034290fe0a2cd0b62e
Gabriel <gabriel.g2d2@gmail.com>
2024-12-25 16:41:02 -0300

    test: add validation for invalid email in ServiceValidationCreateUser

--- src/domain/users/use-cases/create-user/services/validation-create-user/unit.test.ts
+++ src/domain/users/use-cases/create-user/services/validation-create-user/unit.test.ts
@@ -14,6 +14,17 @@ describe("ServiceValidationCreateUser", async () => {
     );
   });
 
-  test("should throw an error if the email is invalid", async () => {});
+  test("should throw an error if the email is invalid", async () => {
+    const name = "jhon";
+    const email = "jhon.com";
+    const password = "123456";
+
+    const service = new ServiceValidationCreateUser();
+
+    await expect(service.execute({ name, email, password })).rejects.toThrow(
+      "Error: Invalid content"
+    );
+  });
+
   test("should throw an error if the password is invalid", async () => {});
 });
