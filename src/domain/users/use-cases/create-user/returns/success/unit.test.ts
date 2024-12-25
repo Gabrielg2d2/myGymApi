94f63df7c8b9c0d77e6bfbc3b8aae3e594ea19e9
Gabriel <gabriel.g2d2@gmail.com>
2024-12-25 19:05:37 -0300

    test: add unit tests for SuccessCreateUser use case

---
+++ src/domain/users/use-cases/create-user/returns/success/unit.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect, test } from "vitest";
+import { SuccessCreateUser } from ".";
+import { IDataResponse } from "../../repository";
+
+describe("SuccessCreateUser", () => {
+  test("should always return a date other than null", async () => {
+    const data: IDataResponse = {
+      id: "1",
+      name: "John Doe",
+      email: "jhon@gmail.com",
+      password_hash: "67c89asca123456cascasc51c65as",
+      created_at: new Date(),
+    };
+
+    const newUser = new SuccessCreateUser().execute(data);
+
+    expect(newUser.data).not.toBe(null);
+  });
+
+  test("should always return an object with the following format", async () => {
+    const data: IDataResponse = {
+      id: new Date().getTime().toString(),
+      name: "John Doe",
+      email: "jhon@gmail.com",
+      password_hash: "67c89asca123456cascasc51c65as",
+      created_at: new Date(),
+    };
+
+    const newUser = new SuccessCreateUser().execute(data);
+
+    expect(newUser).toEqual({
+      data: {
+        id: expect.any(String),
+        name: "John Doe",
+        email: "jhon@gmail.com",
+        password_hash: expect.any(String),
+        created_at: expect.any(Date),
+      },
+      message: {
+        en: "User created successfully",
+        pt: "Usuário criado com sucesso",
+      },
+      typeMessage: "success",
+      statusCode: 201,
+      error: null,
+    });
+  });
+});
