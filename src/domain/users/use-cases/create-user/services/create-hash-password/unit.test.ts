7fa0cf68135c73f1e327370369a1bccf85a20c61
Gabriel <gabriel.g2d2@gmail.com>
2024-12-25 15:22:57 -0300

    test: organize unit tests for ServiceCreateHashPassword into success and error scenarios

--- src/domain/users/use-cases/create-user/services/create-hash-password/unit.test.ts
+++ src/domain/users/use-cases/create-user/services/create-hash-password/unit.test.ts
@@ -3,30 +3,34 @@ import { describe, expect, test } from "vitest";
 import { ServiceCreateHashPassword } from ".";
 
 describe("ServiceCreateHashPassword", async () => {
-  test("should return a hashed password", async () => {
-    const password = "1235678";
-    const hashedPassword = await new ServiceCreateHashPassword().execute(
-      password
-    );
+  describe("Success", async () => {
+    test("should return a hashed password", async () => {
+      const password = "1235678";
+      const hashedPassword = await new ServiceCreateHashPassword().execute(
+        password
+      );
 
-    const isHashed = await compare(password, hashedPassword);
+      const isHashed = await compare(password, hashedPassword);
 
-    expect(isHashed).toBe(true);
-    expect(hashedPassword).toEqual(expect.any(String));
-    expect(hashedPassword).not.toEqual(password);
+      expect(isHashed).toBe(true);
+      expect(hashedPassword).toEqual(expect.any(String));
+      expect(hashedPassword).not.toEqual(password);
+    });
   });
 
-  test("should return an error 'Password is required'", async () => {
-    const passwordEmpty = "";
-    await expect(
-      new ServiceCreateHashPassword().execute(passwordEmpty)
-    ).rejects.toThrow("Password is required");
-  });
+  describe("Error", async () => {
+    test("should return an error 'Password is required'", async () => {
+      const passwordEmpty = "";
+      await expect(
+        new ServiceCreateHashPassword().execute(passwordEmpty)
+      ).rejects.toThrow("Password is required");
+    });
 
-  test("should return an error 'Password must be at least 6 characters'", async () => {
-    const password = "123";
-    await expect(
-      new ServiceCreateHashPassword().execute(password)
-    ).rejects.toThrow("Password must be at least 6 characters");
+    test("should return an error 'Password must be at least 6 characters'", async () => {
+      const password = "123";
+      await expect(
+        new ServiceCreateHashPassword().execute(password)
+      ).rejects.toThrow("Password must be at least 6 characters");
+    });
   });
 });
