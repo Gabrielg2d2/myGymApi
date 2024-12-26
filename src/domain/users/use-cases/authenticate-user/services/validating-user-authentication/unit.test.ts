11070fdd038f4d39c8f8bad64afc598462617b41
Gabriel <gabriel.g2d2@gmail.com>
2024-12-26 10:45:05 -0300

    test: add validation for missing password in ServiceValidationAuthenticateUser

--- src/domain/users/use-cases/authenticate-user/services/validating-user-authentication/unit.test.ts
+++ src/domain/users/use-cases/authenticate-user/services/validating-user-authentication/unit.test.ts
@@ -34,7 +34,21 @@ describe("ServiceValidationAuthenticateUser", () => {
     });
 
     // deveria retornar um erro, caso o password não seja informado
-    test("should return an error if password is not provided", async () => {});
+    test("should return an error if password is not provided", async () => {
+      const sut = new ServiceValidationAuthenticateUser();
+      const user = {
+        email: "jhon@gmail.com",
+        password: "",
+      };
+
+      try {
+        await sut.execute(user);
+      } catch (error) {
+        if (error instanceof Error) {
+          expect(error.message).toBe("Error: Credentials are invalid");
+        }
+      }
+    });
 
     // deveria retornar um erro, caso o password ou email seja inválido
     test("should return an error if password or email is invalid", async () => {});
