29618297ec1bbd91e9e82d2a0cc9d50c178efb7a
Gabriel <gabriel.g2d2@gmail.com>
2024-12-26 18:23:19 -0300

    test: refactor AuthenticateUserUseCase tests to use RepositoryTest for improved mock implementation

--- src/domain/users/use-cases/authenticate-user/main.test.ts
+++ src/domain/users/use-cases/authenticate-user/main.test.ts
@@ -1,25 +1,12 @@
-import { env } from "@/env";
-import bcryptjs from "bcryptjs";
 import { describe, expect, test } from "vitest";
 import { AuthenticateUserUseCase } from "./main";
 import { RepositoryAuthenticateUser } from "./repository";
+import { RepositoryTest } from "./repository/repository-test";
 
 describe("AuthenticateUserUseCase", () => {
   test("should authenticate user", async () => {
-    const passwordHash = await bcryptjs.hash("123123", env.HASH_SALT);
-
-    const mockRepository = {
-      execute: async () => {
-        return {
-          id: new Date().getTime().toString(),
-          name: "Test",
-          email: "test@gmail.com",
-          password_hash: passwordHash,
-          created_at: new Date(),
-        };
-      },
-    } as unknown as RepositoryAuthenticateUser;
-
+    const mockRepository =
+      new RepositoryTest() as unknown as RepositoryAuthenticateUser;
     const sut = new AuthenticateUserUseCase(mockRepository);
 
     const result = await sut.execute({
--- src/domain/users/use-cases/authenticate-user/repository/repository-test/index.ts
+++ src/domain/users/use-cases/authenticate-user/repository/repository-test/index.ts
@@ -1,20 +1,28 @@
 import { AdapterBcryptjs } from "@/domain/adapters/hash/bcryptjs";
+import { env } from "@/env";
 import { IDataRequest, IRepositoryAuthenticateUser, IUser } from "../interface";
 
 export class RepositoryTest implements IRepositoryAuthenticateUser {
+  private password = "123123";
+
   constructor(private readonly adapter = new AdapterBcryptjs()) {}
 
-  users: IUser[] = [
-    {
+  users: IUser[] = [];
+
+  async execute(data: IDataRequest) {
+    const passwordHash = await this.adapter.bcryptjs.hash(
+      this.password,
+      env.HASH_SALT
+    );
+
+    this.users.push({
       id: new Date().getTime().toString(),
       name: "Test",
       email: "test@gmail.com",
-      password_hash: "123123",
+      password_hash: passwordHash,
       created_at: new Date(),
-    },
-  ];
+    });
 
-  async execute(data: IDataRequest) {
     const user = this.users.find((user) => user?.email === data.email);
 
     if (!user) {
