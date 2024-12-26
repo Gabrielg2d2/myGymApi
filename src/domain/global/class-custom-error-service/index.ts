103a538cea9418513e837831aa2b7e2da3f10274
Gabriel <gabriel.g2d2@gmail.com>
2024-12-26 17:42:34 -0300

    feat: add CustomErrorService to handle error responses with standardized messages

---
+++ src/domain/global/class-custom-error-service/index.ts
@@ -0,0 +1,16 @@
+import { ITypeMessageGlobal } from "../types/type-message";
+
+export class CustomErrorService {
+  execute(error?: Error | unknown) {
+    return {
+      data: null,
+      message: {
+        en: "Service unavailable, try again later",
+        pt: "Serviço indisponível, tente novamente mais tarde",
+      },
+      typeMessage: ITypeMessageGlobal.FATAL,
+      statusCode: 500,
+      error: error instanceof Error ? error.message : String(error),
+    };
+  }
+}
