155e0cc089c6e268676a65645e8f699c7fc4e1a8
Gabriel <gabriel.g2d2@gmail.com>
2024-12-26 10:44:22 -0300

    refactor: remove outdated unit tests for user authentication success and error handling

--- src/domain/users/use-cases/authenticate-user/services/validating-user-authentication/unit.test.ts
+++ src/domain/users/use-cases/authenticate-user/services/validating-user-authentication/unit.test.ts
@@ -3,7 +3,6 @@ import { ServiceValidationAuthenticateUser } from ".";
 
 describe("ServiceValidationAuthenticateUser", () => {
   describe("Success", () => {
-    // não deveria retornar nada em caso de erro
     test("should return nothing if user is valid", async () => {
       const sut = new ServiceValidationAuthenticateUser();
       const user = {
@@ -16,5 +15,28 @@ describe("ServiceValidationAuthenticateUser", () => {
     });
   });
 
-  //   describe("Error", () => {});
+  describe("Error", () => {
+    // deveria retornar um erro, caso o email não seja informado
+    test("should return an error if email is not provided", async () => {
+      const sut = new ServiceValidationAuthenticateUser();
+      const user = {
+        email: "",
+        password: "123456",
+      };
+
+      try {
+        await sut.execute(user);
+      } catch (error) {
+        if (error instanceof Error) {
+          expect(error.message).toBe("Error: Credentials are invalid");
+        }
+      }
+    });
+
+    // deveria retornar um erro, caso o password não seja informado
+    test("should return an error if password is not provided", async () => {});
+
+    // deveria retornar um erro, caso o password ou email seja inválido
+    test("should return an error if password or email is invalid", async () => {});
+  });
 });
