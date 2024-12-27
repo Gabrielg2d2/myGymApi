6a973cd5b7c71dfa619305ff6f75b11ab020bb8f
Gabriel <gabriel.g2d2@gmail.com>
2024-12-27 09:22:36 -0300

    refactor: update ValidatingUserAuthentication to return boolean for password validation and simplify error handling

--- src/domain/users/use-cases/authenticate-user/adapters/validation/validating-user-authentication.ts
+++ src/domain/users/use-cases/authenticate-user/adapters/validation/validating-user-authentication.ts
@@ -1,32 +1,23 @@
 import { AdapterBcryptjs } from "@/domain/adapters/hash/bcryptjs";
-import { CustomErrorGlobal } from "@/domain/global/class-custom-error";
 import { IUser } from "../../repository/interface";
 
 interface IValidatingUserAuthentication {
-  execute(user: IUser, password: string): Promise<void>;
+  execute(user: IUser, password: string): Promise<boolean>;
 }
 
-export class ValidatingUserAuthentication
+export class AdapterValidatingUserAuthentication
   implements IValidatingUserAuthentication
 {
   constructor(private readonly adapter = new AdapterBcryptjs()) {}
 
   async execute(user: IUser, password: string) {
-    if (!user || !password)
-      throw new CustomErrorGlobal({
-        message: "Error: Credentials are invalid",
-      });
-
-    const { password_hash } = user;
+    if (!user?.password_hash || !password) return false;
 
     const isPasswordValid = await this.adapter.bcryptjs.compare(
       password,
-      password_hash
+      user.password_hash
     );
 
-    if (!isPasswordValid)
-      throw new CustomErrorGlobal({
-        message: "Error: Credentials are invalid",
-      });
+    return isPasswordValid;
   }
 }
