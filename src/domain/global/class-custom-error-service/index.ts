74c407a3a8dd45899b952a98c79485e927abbcfa
Gabriel <gabriel.g2d2@gmail.com>
2024-12-27 09:06:49 -0300

    docs: add JSDoc comments to CustomErrorService for better documentation and usage examples

--- src/domain/global/class-custom-error-service/index.ts
+++ src/domain/global/class-custom-error-service/index.ts
@@ -1,5 +1,17 @@
 import { ITypeMessageGlobal } from "../types/type-message";
 
+/**
+ *
+ * @description can be used to handle errors globally, allowed to handle only errors
+ *
+ * @example
+ * const customErrorService = new CustomErrorService();
+ * const response = customErrorService.execute(null);
+ *
+ * @returns {object} { data, message, typeMessage, statusCode, error }
+ *
+ *
+ **/
 export class CustomErrorService {
   execute(error: Error | unknown | null) {
     return {
