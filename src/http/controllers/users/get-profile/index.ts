0d252da50f41023d01d7651c6aa2f3e7c3fe12ba
Gabriel <gabriel.g2d2@gmail.com>
2024-12-28 08:31:50 -0300

    feat: update getProfileController to use route parameters and modify route definition for user profile retrieval

--- src/http/controllers/users/get-profile/index.ts
+++ src/http/controllers/users/get-profile/index.ts
@@ -1,13 +1,17 @@
-import { IDataGetProfileRequest, UsersDomain } from "@/domain/users/main";
+import { UsersDomain } from "@/domain/users/main";
 import { FastifyReply, FastifyRequest } from "fastify";
 
+type IParams = {
+  id: string;
+};
+
 export async function getProfileController(
-  request: FastifyRequest<{ Body: IDataGetProfileRequest }>,
+  request: FastifyRequest<{ Params: IParams }>,
   reply: FastifyReply
 ) {
   const domain = new UsersDomain();
 
-  const result = await domain.getProfile(request.body);
+  const result = await domain.getProfile({ userId: request.params.id });
 
   return reply.status(result.statusCode).send(result);
 }
--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { authenticateUserController } from "../controllers/users/authenticate-user";
 import { createUserController } from "../controllers/users/create-user";
+import { getProfileController } from "../controllers/users/get-profile";
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post("/users", createUserController);
   app.post("/session", authenticateUserController);
+  app.get("/profile/:id", getProfileController);
 }
