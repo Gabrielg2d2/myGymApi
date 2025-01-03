1d0e5cac7d1f8baaf54af88ea07e91fd84fb1e11
Gabriel <gabriel.g2d2@gmail.com>
2025-01-03 07:20:47 -0300

    feat: define ICheckIn type and update IRepositoryCheckIn interface for check-in creation

--- src/domain/checkin/repositories/interface.ts
+++ src/domain/checkin/repositories/interface.ts
@@ -1,1 +1,11 @@
-export interface IRepositoryCheckIn {}
+type ICheckIn = {
+  id: string;
+  validated_at: Date | null;
+  created_at: Date;
+  user_id: string;
+  gym_id: string;
+};
+
+export interface IRepositoryCheckIn {
+  create(userId: string, gymId: string): Promise<ICheckIn>;
+}
